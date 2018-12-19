48a2c46bf3a2f55f9c83ccffeff5b63b4fe55fa3
Eduardo Barbosa (Anakein) <37910997+e-anakein@users.noreply.github.com>
2018-12-19 02:09:14 -0300

    Update linpen.sh

--- BASH/linpen.sh
+++ BASH/linpen.sh
@@ -1,6 +1,6 @@
 ########################### LINPEN ######################################################
 # By Anakein										#
-# Name: 	= Linux Enumpen								#
+# Name: 	= LinuxPenetration (Linux Check Privilege Escalation)								#
 # Reference 	= https://blog.g0tmi1k.com/2011/08/basic-linux-privilege-escalation/	#
 # Date		= 12/18/2018								#
 # Usage 	= chmod +x linpen.sh && ./linpen.sh					#
