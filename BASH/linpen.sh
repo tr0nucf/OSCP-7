0dfd61bc748fec97c2ccba591f2c5e18c0996a3e
Eduardo Barbosa (Anakein) <37910997+e-anakein@users.noreply.github.com>
2018-12-18 22:20:21 -0300

    Create linpen.sh

---
+++ BASH/linpen.sh
@@ -0,0 +1,7 @@
+########################### LINPEN ######################################################
+# By Anakein										#
+# Name: 	= Linux Enumpen								#
+# Reference 	= https://blog.g0tmi1k.com/2011/08/basic-linux-privilege-escalation/	#
+# Date		= 12/18/2018								#
+# Usage 	= chmod +x linpen.sh && ./linpen.sh					#
+#########################################################################################
